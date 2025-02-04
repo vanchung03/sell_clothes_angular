9ffc2238861d1cc535c79197fc521659c784b8d5
Chung Lường Văn <chungvann2003@gmail.com>
2025-02-04 13:02:16 +0700

    Save changes before updating Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from '../app/app-routing.module';  // Import AppRout
 import { AppComponent } from '../app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';  // Import ToastrModule
+
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
@@ -13,7 +15,12 @@ import { DashboardComponent } from './admin/dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,  // Đảm bảo AppRoutingModule được thêm vào
     HttpClientModule,  // Đảm bảo HttpClientModule đã được thêm vào
-    BrowserAnimationsModule  // Thêm BrowserAnimationsModule
+    BrowserAnimationsModule,  // Thêm BrowserAnimationsModule
+    ToastrModule.forRoot({  // Khởi tạo ToastrModule
+      positionClass: 'toast-top-right',  // Vị trí thông báo
+      timeOut: 3000,  // Thời gian hiển thị thông báo
+      preventDuplicates: true,  // Ngừng hiển thị thông báo trùng lặp
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';  // Import ToastrService
 import { AuthService } from '../service/auth.service';  
 import { LoginRequest } from '../models/login-request.model';  
 import { Router } from '@angular/router';  
@@ -14,7 +15,7 @@ export class LoginComponent {
   constructor(
     private authService: AuthService,
     private router: Router,
-    
+    private toastr: ToastrService  // Inject ToastrService
   ) {}
 
   onLogin() {
@@ -23,16 +24,17 @@ export class LoginComponent {
         console.log('Response from API:', response);  
         if (response && response.token) {
           this.authService.saveToken(response.token);
-          console.log('Đăng nhập thành công!');
+          this.toastr.success('Đăng nhập thành công!');  // Hiển thị thông báo thành công
     
           // Kiểm tra và chuyển hướng sau khi đăng nhập thành công
           this.authService.navigateBasedOnRoleAndStatus();
         } else {
-          console.error('Không có token trong phản hồi API!');
+          this.toastr.error('Không có token trong phản hồi API!');  // Thông báo lỗi
         }
       },
       (error) => {
         console.log('Lỗi đăng nhập:', error);
+        this.toastr.error('Lỗi đăng nhập!');  // Thông báo lỗi nếu có lỗi xảy ra
       }
     );
   }
